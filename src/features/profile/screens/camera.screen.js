bb9f438bdfd695255655495ec13d868517d6b57a
Cristhine <cristhinetostes@gmail.com>
2023-05-26 19:43:35 -0300

    pagina produto detalhado

--- src/features/profile/screens/camera.screen.js
+++ src/features/profile/screens/camera.screen.js
@@ -1,7 +1,8 @@
 import React, { useRef, useState, useEffect, useContext } from "react";
 import { Camera, requestCameraPermissionsAsync } from "expo-camera";
 import styled from "styled-components";
-import { View, TouchableOpacity } from "react-native";
+import { View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 import { Text } from "../../../components/typography/text.component";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 

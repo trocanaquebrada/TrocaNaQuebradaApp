0db132406b378f7d273f667579359c7adf7e0009
Cristhine <cristhinetostes@gmail.com>
2023-06-22 19:14:16 -0300

    atualizacao pagina de troca

--- src/features/exchanges/screens/trade.screen.js
+++ src/features/exchanges/screens/trade.screen.js
@@ -2,11 +2,31 @@ import React from "react";
 
 import { SafeArea } from "../../../components/utility/safe-area.component";
 import { Text } from "../../../components/typography/text.component";
+import {
+  TradeBackground,
+  TradeContainer,
+  TradeButton,
+} from "../components/trade.styles";
+import { Spacer } from "../../../components/spacer/spacer.component";
 
 export const TradeScreen = ({ navigation }) => {
   return (
-    <SafeArea>
-      <Text> Troca rolando aqui </Text>
-    </SafeArea>
+    <TradeBackground>
+      <SafeArea>
+        <TradeContainer>
+          <Spacer size="large">
+            <Text> Solicitacao de troca enviada </Text>
+          </Spacer>
+          <Spacer size="large">
+            <TradeButton
+              mode="contained"
+              onPress={() => navigation.navigate("ChatScreen")}
+            >
+              Ir para o Chat
+            </TradeButton>
+          </Spacer>
+        </TradeContainer>
+      </SafeArea>
+    </TradeBackground>
   );
 };
